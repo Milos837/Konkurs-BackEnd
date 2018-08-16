dda71560337c17af1144032279752985106d7507
Milos Tepic <milos837@gmail.com>
2018-08-16 12:00:36 +0200

    added roles

--- src/main/java/com/example/konkurs/entities/AdminEntity.java
+++ src/main/java/com/example/konkurs/entities/AdminEntity.java
@@ -1,10 +1,14 @@
 package com.example.konkurs.entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Version;
 
@@ -23,6 +27,10 @@ public class AdminEntity {
 	
 	@Column
 	private String password;
+	
+	@ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+	@JoinColumn(name = "role")
+	private RoleEntity role;
 	
 	@Column
 	private Boolean deleted;
@@ -57,6 +65,14 @@ public class AdminEntity {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public RoleEntity getRole() {
+		return role;
+	}
+
+	public void setRole(RoleEntity role) {
+		this.role = role;
 	}
 
 	public Boolean getDeleted() {
