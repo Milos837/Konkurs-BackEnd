bcaf09d783da44ea2d25e3843ad3db453388a1cf
Milos Tepic <milos837@gmail.com>
2018-08-18 12:21:47 +0200

    added cv upload

--- src/main/java/com/example/konkurs/repositories/CandidateLanguageRepository.java
+++ src/main/java/com/example/konkurs/repositories/CandidateLanguageRepository.java
@@ -1,9 +1,14 @@
 package com.example.konkurs.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.konkurs.entities.CandidateEntity;
 import com.example.konkurs.entities.CandidateLanguageEntity;
 
 public interface CandidateLanguageRepository extends CrudRepository<CandidateLanguageEntity, Integer> {
+	
+	List<CandidateLanguageEntity> findByCandidate(CandidateEntity candidate);
 
 }
--- src/main/java/com/example/konkurs/repositories/EducationRepository.java
+++ src/main/java/com/example/konkurs/repositories/EducationRepository.java
@@ -1,9 +1,14 @@
 package com.example.konkurs.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.konkurs.entities.CandidateEntity;
 import com.example.konkurs.entities.EducationEntity;
 
 public interface EducationRepository extends CrudRepository<EducationEntity, Integer> {
+	
+	List<EducationEntity> findByCandidate(CandidateEntity candidate);
 
 }
